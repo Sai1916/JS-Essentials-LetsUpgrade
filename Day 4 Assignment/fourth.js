75db831217ff03f6cf944ece7b787a6d9c7bf969
Sai Sumedh <52703087+Sai1916@users.noreply.github.com>
2020-07-22 23:45:41 +0530

    Update fourth.js

--- Day 4 Assignment/fourth.js
+++ Day 4 Assignment/fourth.js
@@ -1,36 +1,47 @@
-let input = prompt("Enter values with operator");
-let num1 = input.split(",")[0];
-let num2 = input.split(",")[1];
-console.log(`${num1},${num2}`);
-let choice;
-console.log("Enter your choice of operaition");
-console.log("1.Addition");
-console.log("2.Subtraction");
-console.log("3.Multiplication");
-console.log("4.Division");
-console.log("5.SqareRoot");
-console.log("6.Percentage");
+function add() {
+  let num1 = document.getElementById("input1").value;
+  let num2 = document.getElementById("input2").value;
+  let result = document.getElementById("res");
+  let res1 = "The Addition of " + num1 + " and " + num2 + " is ";
+  result.innerHTML = res1 + String(parseInt(num1) + parseInt(num2));
+}
 
-choice = prompt("Enter your choice of operaition");
-while (true) {
-  switch (choice) {
-    case 1:
-      let res = eval(num1 + num2);
-      console.log(`Addition of ${num1} and ${num2} is ${num1}+${num2}`);
-    case 2:
-      console.log(`Subtraction of ${num1} and ${num2} is ${num1}-${num2}`);
-    case 3:
-      console.log(`Multiplication of ${num1} and ${num2} is ${num1}*${num2}`);
-    case 4:
-      console.log(`Division of ${num1} and ${num2} is ${num1}/${num2}`);
-    case 5:
-      let result1 = Math.sqrt(num1);
-      let result2 = Math.sqrt(num2);
-      console.log(
-        `Sqareroot of ${num1} and ${num2} is ${result1} and ${result2}`
-      );
-    case 6:
-      let res = (num1 / num2) * 100;
-      console.log(`Percentage of ${num1} and ${num2} is ${res}`);
-  }
+function subtract() {
+  let num1 = document.getElementById("input1").value;
+  let num2 = document.getElementById("input2").value;
+  let result = document.getElementById("res");
+  let res1 = "The Subtraction of " + num1 + " and " + num2 + " is ";
+  result.innerHTML = res1 + String(parseInt(num1) - parseInt(num2));
+}
+
+function division() {
+  let num1 = document.getElementById("input1").value;
+  let num2 = document.getElementById("input2").value;
+  let result = document.getElementById("res");
+  let res1 = "The Division of " + num1 + " and " + num2 + " is ";
+  result.innerHTML = res1 + String(parseInt(num1) / parseInt(num2));
+}
+
+function multiply() {
+  let num1 = document.getElementById("input1").value;
+  let num2 = document.getElementById("input2").value;
+  let result = document.getElementById("res");
+  let res1 = "The Multiplication of " + num1 + " and " + num2 + " is ";
+  result.innerHTML = res1 + String(parseInt(num1) * parseInt(num2));
+}
+
+function squareRoot() {
+  let num1 = document.getElementById("input1").value;
+  let num2 = document.getElementById("input2").value;
+  let result = document.getElementById("res");
+  let res1 = "The Square Root of " + num1 + " is ";
+  result.innerHTML = res1 + String(Math.sqrt(parseInt(num1)));
+}
+
+function percentage() {
+  let num1 = document.getElementById("input1").value;
+  let num2 = document.getElementById("input2").value;
+  let result = document.getElementById("res");
+  let res1 = "The Percentage of " + num1 + " and " + num2 + " is ";
+  result.innerHTML = res1 + String((parseInt(num1) / 100) * parseInt(num2));
 }
